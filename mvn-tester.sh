35b9d9e4371944f2da1317c63ffa30a19ac131ca
Yusuf Pisan <pisan@uw.edu>
2021-06-04 12:05:55 -0700

    quiet

--- mvn-tester.sh
+++ mvn-tester.sh
@@ -7,7 +7,7 @@ cp HelloWorld.java ./hello-world-test/src/main/java/
 cd hello-world-test
 
 echo "============================================== mvn clean compile test --batch-mode --fail-at-end"
-mvn clean compile test --batch-mode --fail-at-end
+mvn clean compile test --quiet --no-transfer-progress --batch-mode --fail-at-end
 
 echo "============================================== mvn surefire-report:report -DshowSuccess=true"
 mvn surefire-report:report -DshowSuccess=true
