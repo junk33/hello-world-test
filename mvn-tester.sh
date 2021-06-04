78866999869f98fc4c2e3c4d32b8bc92e8711398
Yusuf Pisan <pisan@uw.edu>
2021-06-04 12:10:19 -0700

    with klr

--- mvn-tester.sh
+++ mvn-tester.sh
@@ -11,3 +11,5 @@ mvn clean compile test --quiet --no-transfer-progress --batch-mode --fail-at-end
 
 echo "============================================== mvn surefire-report:report -DshowSuccess=true"
 mvn --quiet --no-transfer-progress surefire-report:report -DshowSuccess=true
+
+ls -lR
