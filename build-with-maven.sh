2e8de501cb71b1081f4b4b3f79caa9747a445c90
Yusuf Pisan <pisan@uw.edu>
2021-06-04 20:13:44 -0700

    s1

---
+++ build-with-maven.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Running build-with-maven.sh script"
+
+
+rm -rf hello-world-test
+git clone --quiet https://github.com/junk33/hello-world-test.git
+
+rm -rf hello-world-test/target
+cp HelloWorld.java hello-world-test/src/main/java
+cd hello-world-test
+# --fail-never
+# --fail-at-end
+mvn clean compile test --quiet --no-transfer-progress --batch-mode --fail-never
+
+mkdir out
+echo "Hello World" out/file1.txt
+
+
+# java -classpath junit-4.13.2.jar:.:target/classes HelloWorld
+
